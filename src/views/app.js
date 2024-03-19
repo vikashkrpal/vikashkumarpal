70e7d9044171ad0e87d8b0352c2254b33de5ec45
deepak <structlooper@gmail.com>
2024-03-19 16:15:41 +0530

    code base | bootstrap@5

---
+++ src/views/app.js
@@ -0,0 +1,29 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import  '../utils/styles/global.css';
+
+const App = () => {
+  return (
+    <main className=" mainContainer">
+      <div className="container text-light">
+        <div className="row">
+          <div className="col">
+            <h1>Hello, Bootstrap 5 in Next.js!</h1>
+            <h1>Hello, Bootstrap 5 in Next.js!</h1>
+            <h1>Hello, Bootstrap 5 in Next.js!</h1>
+            <h1>Hello, Bootstrap 5 in Next.js!</h1>
+            <h1>Hello, Bootstrap 5 in Next.js!</h1>
+            <h1>Hello, Bootstrap 5 in Next.js!</h1>
+            <h1>Hello, Bootstrap 5 in Next.js!</h1>
+            <h1>Hello, Bootstrap 5 in Next.js!</h1>
+            <h1>Hello, Bootstrap 5 in Next.js!</h1>
+            <h1>Hello, Bootstrap 5 in Next.js!</h1>
+            <h1>Hello, Bootstrap 5 in Next.js!</h1>
+            <button className="btn btn-primary">Primary Button</button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default App;
